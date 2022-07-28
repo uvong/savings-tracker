c0d281ba5420b164fe0a39b43f5eb37300dae071
Uyen Vong <uvong92@gmail.com>
2022-07-28 10:14:12 -0700

    formats goal created date

--- src/Goal.js
+++ src/Goal.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function Goal(props) {
+  const dateCreated = props.dateCreated.toDate();
   return (
     <div>
       <h1> Name: {props.name} </h1>
       <h1> Total Amount: ${props.totalAmount}</h1>
       <h1> Goal reached: {String(props.isReached)} </h1>
-      <div> Date created: {String(props.dateCreated.toDate())} </div>
+      <div> Date created: {dateCreated.toDateString()} </div>
     </div>
   );
 }

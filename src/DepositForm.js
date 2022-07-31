68098dcdd9b78b36b56d0fa3bfea0bd526d67697
MandyG415 <missmandygao@gmail.com>
2022-07-31 15:44:05 -0700

    Pseudocode on summing goal amounts, and validating amount input is less than goal amount

--- src/DepositForm.js
+++ src/DepositForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { addDoc } from "firebase/firestore";
 import { useState } from "react";
+import App from "./App";
 
 const DepositForm = (props) => {
   const [newAmount, setNewAmount] = useState(0);
@@ -12,6 +13,17 @@ const DepositForm = (props) => {
     });
     props.getDeposits(props.id);
   };
+
+  // // const validateDepositAmount = () => {
+  //   if (props.totalAmount <  amount) {
+      //console.log("amount too large")
+      // can have a red textbox here or make submit button unable to click
+  //   }
+  // }
+
+  // const sumDepositAmount = () => {
+  //   map to sum amount, not sure if here or in App.js
+  // }
   return (
     <div>
       <input

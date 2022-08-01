3cea246ae68210be1819b2d9156c7a4e885c93d5
MandyG415 <missmandygao@gmail.com>
2022-08-01 12:31:11 -0700

    Added sumTotal for each Deposit component, need to have it accurately reflect after each deposit.

--- src/DepositList.js
+++ src/DepositList.js
@@ -2,8 +2,19 @@ import React from "react";
 import Deposit from "./Deposit";
 
 function DepositList(props) {
+  const sumDepositAmount = (deposits) => {
+    //map to sum amount, not sure if here or in App.js
+    //expected output: 19
+    let sumTotal = 0;
+    for (const deposit of deposits) {
+      sumTotal += deposit.amount;
+    }
+    return sumTotal;
+  };
+
   return (
     <div>
+      <h1>Current Sum: {sumDepositAmount(props.deposits)} </h1>
       {props.deposits.map((deposit) => {
         return (
           <div>
@@ -12,6 +23,7 @@ function DepositList(props) {
               id={deposit.id}
               amount={deposit.amount}
               depositDate={deposit.depositDate}
+              sumTotal = {sumDepositAmount(props.deposits)}
             />
           </div>
         );
